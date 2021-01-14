b55b20310cc3e0d9abdf4d02cbe13718f9e128be
HibanaSama <48864283+HibanaSama@users.noreply.github.com>
2021-01-14 20:20:08 +0500

    Sidebar and Navbar in external files
    Credits to @Mirdukkk for help with this technical part

---
+++ src/.vuepress/configs/navbar.js
@@ -0,0 +1,33 @@
+const navbar = [
+    {
+        text: 'Руководства от Dortania',
+        ariaLabel: 'Language Menu',
+        items: [{
+            text: 'Домашняя страница',
+            link: 'https://dortania.github.io/'
+        },
+        {
+            text: 'Начало работы с ACPI',
+            link: 'https://dortania.github.io/Getting-Started-With-ACPI/'
+        },
+        {
+            text: 'После установки OpenCore',
+            link: 'https://dortania.github.io/OpenCore-Post-Install/'
+        },
+        {
+            text: 'GPU Buyers Guide',
+            link: 'https://dortania.github.io/GPU-Buyers-Guide/'
+        },
+        {
+            text: 'Wireless Buyers Guide',
+            link: 'https://dortania.github.io/Wireless-Buyers-Guide/'
+        },
+        {
+            text: 'Anti Buyers Guide',
+            link: 'https://dortania.github.io/Anti-Hackintosh-Buyers-Guide/'
+        },
+        ]
+    }
+]
+
+module.exports = navbar
